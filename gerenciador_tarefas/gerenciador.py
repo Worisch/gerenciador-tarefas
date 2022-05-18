8003082a869dded6f7fc037b379532cc33f6e5a8
Worisch <brunowlopes88@gmail.com>
2022-05-18 19:10:39 -0300

    Adiciona recurso de listar tarefas

---
+++ gerenciador_tarefas/gerenciador.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+TAREFAS = [
+        {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "titulo": "titulo 1",
+            "descricao": "descricao 1",
+            "estado": "finalizado",
+        }
+]
+
+@app.get("/tarefas")
+def listar():
+    return TAREFAS
